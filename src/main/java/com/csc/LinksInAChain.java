65fb0b0635a31be94ceb6939b98eccfe4f9b2890
Owen Ceresa <142851013+CO84635@users.noreply.github.com>
2024-12-08 11:21:23 -0600

    +Objects & Tests

--- src/main/java/com/csc/LinksInAChain.java
+++ src/main/java/com/csc/LinksInAChain.java
@@ -15,13 +15,25 @@ public class LinksInAChain {
     static class LinkedList<T> {
         Node<T> head;
 
-        private int size() {
+        private int size_total() {
             int size = 0;
             Node<T> current = head;
             while (current != null) {
+                current = current.next;
                 size++;
+            }
+            return size;
+        }
+
+        public int size(Node<T> node) {
+            int size = 0;
+            Node<T> current = node.next;
+
+            while (current != null) {
+                ++size;
                 current = current.next;
             }
+
             return size;
         }
 
@@ -40,7 +52,7 @@ public class LinksInAChain {
         }
 
         public T get(int index) {
-            if (index < 0 || index >= size()) {
+            if (index < 0 || index >= size_total()) {
                 return null;
             }
 
@@ -60,21 +72,141 @@ public class LinksInAChain {
         }
     }
 
+    static class StrawHats {
+        public String name;
+
+        StrawHats(String name) {
+            this.name = name;
+        }
+    } 
+
+    static class ComputerParts {
+        public String part;
+
+        ComputerParts(String part){
+            this.part = part;
+        }
+    }
+
     public static void main(String[] args) {
-        LinkedList<ChainLink> linkedList = new LinkedList<>();
+        {
+            LinkedList<ChainLink> linkedList = new LinkedList<>();
 
-        ChainLink linkOne = new ChainLink("red");
-        ChainLink linkTwo = new ChainLink("blue");
-        ChainLink linkThree = new ChainLink("green");
+            ChainLink linkOne = new ChainLink("red");
+            ChainLink linkTwo = new ChainLink("blue");
+            ChainLink linkThree = new ChainLink("green");
 
-        linkedList.add(linkOne);
-        linkedList.add(linkTwo);
-        linkedList.add(linkThree);
+            linkedList.add(linkOne);
+            linkedList.add(linkTwo);
+            linkedList.add(linkThree);
 
-       ChainLink firstLink = linkedList.get(0);
-       ChainLink secondLink = linkedList.get(1);
+            ChainLink firstLink = linkedList.get(0);
+            ChainLink secondLink = linkedList.get(1);
 
-       System.out.println("First link color: " + firstLink.color);
-       System.out.println("Second link color: " + secondLink.color);
+            System.out.println("First link color: " + firstLink.color);
+            System.out.println("Second link color: " + secondLink.color);
+        }
+
+        {
+            LinkedList<StrawHats> linkedList = new LinkedList<>();
+
+            StrawHats linkOne = new StrawHats("Luffy");
+            StrawHats linkTwo = new StrawHats("Zoro");
+            StrawHats linkThree = new StrawHats("Nami");
+            StrawHats linkFour = new StrawHats("Usopp");
+            StrawHats linkFive = new StrawHats("Sanji");
+            StrawHats linkSix = new StrawHats("Chopper");
+            StrawHats linkSeven = new StrawHats("Robin");
+            StrawHats linkEight = new StrawHats("Franky");
+            StrawHats linkNine = new StrawHats("Brook");
+            StrawHats linkTen = new StrawHats("Jinbei");
+
+            linkedList.add(linkOne);
+            linkedList.add(linkTwo);
+            linkedList.add(linkThree);
+            linkedList.add(linkFour);
+            linkedList.add(linkFive);
+            linkedList.add(linkSix);
+            linkedList.add(linkSeven);
+            linkedList.add(linkEight);
+            linkedList.add(linkNine);
+            linkedList.add(linkTen);
+
+            StrawHats firstLink = linkedList.get(0);
+            StrawHats secondLink = linkedList.get(1);
+            StrawHats thirdLink = linkedList.get(2);
+            StrawHats fourthLink = linkedList.get(3);
+            StrawHats fifthLink = linkedList.get(4);
+            StrawHats sixthLink = linkedList.get(5);
+            StrawHats seventhLink = linkedList.get(6);
+            StrawHats eigthLink = linkedList.get(7);
+            StrawHats ninthLink = linkedList.get(8);
+            StrawHats tenthLink = linkedList.get(9);
+
+            System.out.println("First link Strawhat: " + firstLink.name);
+            System.out.println("Second link Strawhat: " + secondLink.name);
+            System.out.println("Third link Strawhat: " + thirdLink.name);
+            System.out.println("Fourth link Strawhat: " + fourthLink.name);
+            System.out.println("Fifth link Strawhat: " + fifthLink.name);
+            System.out.println("Sixth link Strawhat: " + sixthLink.name);
+            System.out.println("Seventh link Strawhat: " + seventhLink.name);
+            System.out.println("Eigth link Strawhat: " + eigthLink.name);
+            System.out.println("Ninth link Strawhat: " + ninthLink.name);
+            System.out.println("Tenth link Strawhat: " + tenthLink.name);
+
+            Node<StrawHats> nodeForLuffy = linkedList.head;
+            System.out.println("Testing Size method. Anticipating 9 Strawhats after luffy joined: " + linkedList.size(nodeForLuffy));
+        }   
+
+        {
+            LinkedList<ComputerParts> linkedList = new LinkedList<>();
+
+            ComputerParts partOne = new ComputerParts("CPU");
+            ComputerParts partTwo = new ComputerParts("GPU"); 
+            ComputerParts partThree = new ComputerParts("RAM");
+            ComputerParts partFour = new ComputerParts("Motherboard");
+            ComputerParts partFive = new ComputerParts("SSD");
+            ComputerParts partSix = new ComputerParts("Hard Drive");
+            ComputerParts partSeven = new ComputerParts("Power Supply");
+            ComputerParts partEight = new ComputerParts("Cooling Fan");
+            ComputerParts partNine = new ComputerParts("Case");
+            ComputerParts partTen = new ComputerParts("Optical Drive");
+
+            linkedList.add(partOne);
+            linkedList.add(partTwo);
+            linkedList.add(partThree);
+            linkedList.add(partFour);
+            linkedList.add(partFive);
+            linkedList.add(partSix);
+            linkedList.add(partSeven);
+            linkedList.add(partEight);
+            linkedList.add(partNine);
+            linkedList.add(partTen);
+
+            ComputerParts firstPart = linkedList.get(0);
+            ComputerParts secondPart = linkedList.get(1);
+            ComputerParts thirdPart = linkedList.get(2);
+            ComputerParts fourthPart = linkedList.get(3);
+            ComputerParts fifthPart = linkedList.get(4);
+            ComputerParts sixthPart = linkedList.get(5);
+            ComputerParts seventhPart = linkedList.get(6);
+            ComputerParts eigthPart = linkedList.get(7);
+            ComputerParts ninthPart = linkedList.get(8);
+            ComputerParts tenthPart = linkedList.get(9);
+
+            System.out.println("First part: " + firstPart.part);
+            System.out.println("Second part: " + secondPart.part);
+            System.out.println("Third part: " + thirdPart.part);
+            System.out.println("Fourth part: " + fourthPart.part);
+            System.out.println("Fifth part: " + fifthPart.part);
+            System.out.println("Sixth part: " + sixthPart.part);
+            System.out.println("Seventh part: " + seventhPart.part);
+            System.out.println("Eighth part: " + eigthPart.part);
+            System.out.println("Ninth part: " + ninthPart.part);
+            System.out.println("Tenth part: " + tenthPart.part);
+
+            Node<ComputerParts> nodeForCpu = linkedList.head;
+            System.out.println("Size after CPU: " + linkedList.size(nodeForCpu));
+        }
     }
 }
--- src/test/java/com/csc/LinksInAChainTest.java
+++ src/test/java/com/csc/LinksInAChainTest.java
@@ -4,13 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.csc.LinksInAChain.ChainLink;
+import com.csc.LinksInAChain.ComputerParts;
+import com.csc.LinksInAChain.StrawHats;
+
 class LinksInAChainTest {
 
     LinksInAChain.LinkedList<LinksInAChain.ChainLink> links;
+    LinksInAChain.LinkedList<LinksInAChain.StrawHats> strawhats;
+    LinksInAChain.LinkedList<LinksInAChain.ComputerParts> parts;
 
     @BeforeEach
     void setup(){
         links = new LinksInAChain.LinkedList<>();
+        strawhats = new LinksInAChain.LinkedList<>();
+        parts = new LinksInAChain.LinkedList<>();
     }
 
     @Test
@@ -22,7 +30,6 @@ class LinksInAChainTest {
     void TestOneLink() {
         LinksInAChain.ChainLink blueLink = new LinksInAChain.ChainLink("Blue");
         links.add(blueLink);
-
         assertEquals("Blue", links.get(0).color);
     }
 
@@ -31,21 +38,149 @@ class LinksInAChainTest {
         LinksInAChain.ChainLink greenLink = new LinksInAChain.ChainLink("Green");
         LinksInAChain.ChainLink redLink = new LinksInAChain.ChainLink("Red");
         LinksInAChain.ChainLink blueLink = new LinksInAChain.ChainLink("Blue");
-        
         links.add(greenLink);
         links.add(redLink);
         links.add(blueLink);
-        
         assertEquals("Green", links.get(0).color);
         assertEquals("Red", links.get(1).color);
         assertEquals("Blue", links.get(2).color);
     }
 
     @Test
+    void TestSizeOnLinks(){
+        LinksInAChain.ChainLink greenLink = new LinksInAChain.ChainLink("Green");
+        LinksInAChain.ChainLink redLink = new LinksInAChain.ChainLink("Red");
+        LinksInAChain.ChainLink blueLink = new LinksInAChain.ChainLink("Blue");
+        links.add(greenLink);
+        links.add(redLink);
+        links.add(blueLink);
+        LinksInAChain.Node<ChainLink> nodeForRed = links.head;
+        assertEquals(2, links.size(nodeForRed));
+    }
+
+    @Test
     void testOutOfBounds () {
         LinksInAChain.ChainLink greenLink = new LinksInAChain.ChainLink("Green");
         links.add(greenLink);
-        
         assertNull(links.get(1));
+    }
+
+    @Test
+    void testSizeFromLastNode() {
+        LinksInAChain.ChainLink lastLink = new LinksInAChain.ChainLink("Last");
+        links.add(lastLink);
+        LinksInAChain.Node<LinksInAChain.ChainLink> lastNode = links.head;
+
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+
+        assertEquals(0, links.size(lastNode)); 
+    }
+
+    @Test
+    void testSizeFromEmptyNode() {
+        LinksInAChain.Node<LinksInAChain.ChainLink> node = links.head;
+        assertEquals(0, links.size(node));
+    }
+
+    @Test
+    void testZeroStrawhats() {
+        assertNull(strawhats.get(0));
+    }
+
+    @Test
+    void testOneStrawhat() {
+        LinksInAChain.StrawHats luffy = new LinksInAChain.StrawHats("Luffy");
+        strawhats.add(luffy);
+        assertEquals("Luffy", strawhats.get(0).name);
+    }
+
+    @Test
+    void testMultipleStrawhats() {
+        LinksInAChain.StrawHats luffy = new LinksInAChain.StrawHats("Luffy");
+        LinksInAChain.StrawHats zoro = new LinksInAChain.StrawHats("Zoro");
+        LinksInAChain.StrawHats nami = new LinksInAChain.StrawHats("Nami");
+        strawhats.add(luffy);
+        strawhats.add(zoro);
+        strawhats.add(nami);
+        assertEquals("Luffy", strawhats.get(0).name);
+        assertEquals("Zoro", strawhats.get(1).name);
+        assertEquals("Nami", strawhats.get(2).name);
+    }
+
+    @Test
+    void testSizeOnStrawhats() {
+        LinksInAChain.StrawHats luffy = new LinksInAChain.StrawHats("Luffy");
+        LinksInAChain.StrawHats zoro = new LinksInAChain.StrawHats("Zoro");
+        LinksInAChain.StrawHats nami = new LinksInAChain.StrawHats("Nami");
+        strawhats.add(luffy);
+        strawhats.add(zoro);
+        strawhats.add(nami);
+        LinksInAChain.Node<StrawHats> nodeForZoro = strawhats.head;
+        assertEquals(2, strawhats.size(nodeForZoro));
+    }
+
+    @Test
+        void testSizeOnEmptyStrawhats() {
+        LinksInAChain.Node<StrawHats> emptyNode = strawhats.head;
+        assertEquals(0, strawhats.size(emptyNode));
+    }
+
+    @Test
+    void testOutOfBoundsStrawhats() {
+        LinksInAChain.StrawHats luffy = new LinksInAChain.StrawHats("Luffy");
+        strawhats.add(luffy);
+        assertNull(strawhats.get(1));
+    }
+
+    @Test
+    void testZeroParts() {
+        assertNull(parts.get(0));
+    }
+
+    @Test
+    void testOnePart() {
+        LinksInAChain.ComputerParts cpu = new LinksInAChain.ComputerParts("CPU");
+        parts.add(cpu);
+        assertEquals("CPU", parts.get(0).part);
+    }
+
+    @Test
+    void testMultipleParts() {
+        LinksInAChain.ComputerParts cpu = new LinksInAChain.ComputerParts("CPU");
+        LinksInAChain.ComputerParts gpu = new LinksInAChain.ComputerParts("GPU");
+        LinksInAChain.ComputerParts ram = new LinksInAChain.ComputerParts("RAM");
+        parts.add(cpu);
+        parts.add(gpu);
+        parts.add(ram);
+        assertEquals("CPU", parts.get(0).part);
+        assertEquals("GPU", parts.get(1).part);
+        assertEquals("RAM", parts.get(2).part);
+    }
+
+    @Test
+    void testSizeOnParts() {
+        LinksInAChain.ComputerParts cpu = new LinksInAChain.ComputerParts("CPU");
+        LinksInAChain.ComputerParts gpu = new LinksInAChain.ComputerParts("GPU");
+        LinksInAChain.ComputerParts ram = new LinksInAChain.ComputerParts("RAM");
+        parts.add(cpu);
+        parts.add(gpu);
+        parts.add(ram);
+        LinksInAChain.Node<ComputerParts> nodeForGPU = parts.head;
+        assertEquals(2, parts.size(nodeForGPU));
+    }
+
+    @Test
+    void testOutOfBoundsParts() {
+        LinksInAChain.ComputerParts cpu = new LinksInAChain.ComputerParts("CPU");
+        parts.add(cpu);
+        assertNull(parts.get(1));
+    }
+
+    @Test
+    void testSizeOnEmptyParts() {
+        LinksInAChain.Node<ComputerParts> emptyNode = parts.head;
+        assertEquals(0, parts.size(emptyNode));
     }
 }
